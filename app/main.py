40c988ccae2f291494e6b9b328f6d3435d68d045
Henrique Amaral <henriquefernandes1509@gmail.com>
2025-05-16 10:51:09 -0300

    Teste do railway do deploy

--- app/main.py
+++ app/main.py
@@ -6,13 +6,3 @@ app = FastAPI(title="API EMBRAPA - Raspagem de Tabelas")
 
 app.include_router(login_router)
 app.include_router(tabelas_router)
-
-#from fastapi import FastAPI
-#from app.api.acesso import router as login_router
-#from app.api.tabelas import router as tabelas_router
-
-#app = FastAPI(title="API EMBRAPA - Raspagem de Tabelas")
-
-#if __name__ == "__main__":
-#    port = int(os.environ.get("PORT", 10000))
-#    uvicorn.run(app, host="0.0.0.0", port=port)
