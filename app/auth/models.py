f06eae74b61792c0f555a4af579ebed56a9f1c5f
Henrique Amaral <henriquefernandes1509@gmail.com>
2025-05-26 21:47:46 -0300

    Conformidade - merge

--- app/auth/models.py
+++
@@ -1,9 +0,0 @@
-from .security import gerar_hash_senha
-
-# Em produção, use banco real
-usuarios_fake = {
-    "admin": {
-        "username": "admin",
-        "senha_hash": gerar_hash_senha("admin123")  # ou já salve a hash pronta
-    }
-}
